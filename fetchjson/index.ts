38e4d5097484957071abf5fcf4e21cbc83f63ef6
Alan Leverenz <awleverenz@aol.com>
2021-06-02 10:54:01 -0400

    corrected type errors

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -15,9 +15,13 @@ axios.get(url).then(response => {
   const title = todo.title;
   const completed = todo.completed;
 
-  console.log(`
+  logTodo(id, title, completed);
+});
+
+  const logTodo = (id: number, title: string, completed: boolean) => {
+    console.log(`
     The TODO with id: ${id}
     Has a title of: ${title}
     Is it finished? ${completed}
   `);
-});
+  }
