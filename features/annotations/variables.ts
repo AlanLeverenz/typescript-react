15a9e006806ee87515c3b68ee631c55d632bb28b
Alan Leverenz <awleverenz@aol.com>
2021-06-02 16:27:06 -0400

    type annotations

---
+++ features/annotations/variables.ts
@@ -0,0 +1,25 @@
+// assigning an annotation to a value
+let apples: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// built in objects with defined instances
+let now: Date = new Date();
+
+// Array (type of array =  array of strings)
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1,2,3];
+let truths: boolean[] = [true, true, false];
+
+// Classes
+class Car {}
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20
+};
+
